f0844e45d69e0fd6cb874aad9e9ab1754ff5cf20
Shihab <shihabahmedcse25@gmail.com>
2023-03-09 09:39:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const color = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+
+body.style.background = 'violet';
+button.addEventListener('click', changeB);
+
+function changeB() {
+  const colorIndex = parseInt(Math.random() * color.length);
+  body.style.background = color[colorIndex];
+}
